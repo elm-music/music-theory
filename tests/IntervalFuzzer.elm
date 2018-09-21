4623e5f094c18ca1633470caf5a6a6cc36940d55
Leif Battermann <leifbattermann@gmail.com>
2018-09-21 22:07:01 +0200

    Add the original tests (#22)
    * moved helper module from original repo and upgrade to elm 0.19
    
    * added interval tests
    
    * removed placeholder test
    
    * test description
    
    * added pitch class tests

---
+++ tests/IntervalFuzzer.elm
@@ -0,0 +1,19 @@
+module IntervalFuzzer exposing (intervalFuzzer)
+
+import Fuzz exposing (Fuzzer)
+import List.Extra
+import MusicTheory.Interval as Interval
+
+
+intervalFuzzer : Fuzzer Interval.Interval
+intervalFuzzer =
+    Fuzz.intRange 0 ((Interval.all |> List.length) - 1)
+        |> Fuzz.map
+            (\n ->
+                case Interval.all |> List.Extra.getAt n of
+                    Just interval ->
+                        interval
+
+                    Nothing ->
+                        Debug.todo "impossible"
+            )
