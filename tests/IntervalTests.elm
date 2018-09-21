77117355dc947723cd26f1945aedc058e62eb756
Leif Battermann <leif.battermann@gmail.com>
2018-09-21 21:32:49 +0200

    test description

--- tests/IntervalTests.elm
+++ tests/IntervalTests.elm
@@ -11,12 +11,12 @@ import Test exposing (..)
 all : Test
 all =
     describe "Interval Tests"
-        [ test "A test" <|
+        [ test "sum of semitones of an interval and it's complementary interval should be 12" <|
             \_ ->
                 Interval.all
                     |> List.map (\interval -> ( Interval.semitones interval, Interval.complementary interval |> Interval.semitones ))
                     |> List.all (\( a, b ) -> a + b == 12)
-                    |> Expect.true "dflkfdskdfk"
+                    |> Expect.true "sum should be 12"
         , test "complementary interval of a perfect union should be a perfect octave" <|
             \_ ->
                 Interval.perfectUnison |> Interval.complementary |> Expect.equal Interval.perfectOctave
