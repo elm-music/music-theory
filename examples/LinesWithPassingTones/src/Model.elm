9063f95c26c9970b57a0916c661795d6b65805e6
Duncan Malashock <duncanmalashock@gmail.com>
2018-09-22 23:16:18 -0400

    Remove key param, move starting degree into Pattern

--- examples/LinesWithPassingTones/src/Model.elm
+++ examples/LinesWithPassingTones/src/Model.elm
@@ -1,14 +1,16 @@
 module Model exposing (Model, Msg(..), init, update)
 
-import MusicTheory.Key
 import MusicTheory.Pitch exposing (Octave, Pitch)
 import MusicTheory.PitchClass exposing (Accidental(..), Letter(..))
 import MusicTheory.Scale
     exposing
-        ( degree
+        ( Pattern
+        , degree
         , downTo
         , loweredDegree
+        , pattern
         , raisedDegree
+        , startAt
         , upTo
         )
 
@@ -24,7 +26,7 @@ type Msg
 
 {- Ideally, the signature of Scale.generateLine is:
 
-   generateLine : Pattern -> Degree -> Octave -> Maybe Key -> List Pitch
+   generateLine : Pattern -> Octave -> List Pitch
 
    and the call to it below in myLineWithPassingTones returns:
 
@@ -38,7 +40,7 @@ type Msg
        , Pitch (PitchClass E Natural) 4
        ]
 
-   Where this list of pitches is correctly spelled in terms of the key of
+   Where this list of pitches is correctly spelled in terms of the scale of
    C major, uses sharps for raised degrees and flats for lowered degrees,
    begins on the correct scale degree and octave, and moves in the directions
    specified by the Pattern.
@@ -51,28 +53,24 @@ myLineWithPassingTones =
         startingOctave =
             octave 4
 
-        startingDegree =
-            degree 5
-
         scale =
-            Scale.major <| pitch C Natural
-
-        key =
-            Key.major <| C Natural
+            Scale.major <| C Natural
     in
-    Scale.generateLine myPattern startingDegree startingOctave (Just key)
+    Scale.generateLine myPattern startingOctave
 
 
-myPattern : Scale.Pattern
+myPattern : Pattern
 myPattern =
-    [ upTo (raisedDegree 5)
-    , upTo (degree 6)
-    , upTo (degree 1)
-    , downTo (degree 6)
-    , downTo (loweredDegree 6)
-    , downTo (degree 5)
-    , downTo (degree 3)
-    ]
+    pattern
+        [ startAt (degree 5)
+        , upTo (raisedDegree 5)
+        , upTo (degree 6)
+        , upTo (degree 1)
+        , downTo (degree 6)
+        , downTo (loweredDegree 6)
+        , downTo (degree 5)
+        , downTo (degree 3)
+        ]
 
 
 init : Model
