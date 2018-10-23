3628e80992ed3f5b4267b641b7e6ce8157f4a543
Leif Battermann <leif.battermann@gmail.com>
2018-10-23 23:01:30 +0200

    better alias names

--- src/MusicTheory/PitchClass/Enharmonic.elm
+++ src/MusicTheory/PitchClass/Enharmonic.elm
@@ -1,15 +1,15 @@
 module MusicTheory.PitchClass.Enharmonic exposing (asNaturalOrElseFlat, asNaturalOrElseSharp, equivalents, simple)
 
-import MusicTheory.Internal.PitchClass as PitchClassInternal exposing (Offset(..), PitchClass(..))
-import MusicTheory.Internal.PitchClass.Enharmonic as EnharmonicInternal exposing (NaturalOrSingleAccidental(..))
+import MusicTheory.Internal.PitchClass as PitchClass exposing (Offset(..), PitchClass(..))
+import MusicTheory.Internal.PitchClass.Enharmonic as Enharmonic exposing (NaturalOrSingleAccidental(..))
 
 
 simple : PitchClass -> PitchClass
 simple pc =
-    if PitchClassInternal.offset pc == 0 then
+    if PitchClass.offset pc == 0 then
         pc
 
-    else if PitchClassInternal.offset pc < 0 then
+    else if PitchClass.offset pc < 0 then
         asNaturalOrElseFlat pc
 
     else
@@ -18,24 +18,24 @@ simple pc =
 
 asNaturalOrElseFlat : PitchClass -> PitchClass
 asNaturalOrElseFlat pc =
-    case pc |> PitchClassInternal.semitones |> EnharmonicInternal.semitonesToNaturalOrAccidental of
+    case pc |> PitchClass.semitones |> Enharmonic.semitonesToNaturalOrAccidental of
         Nat letter ->
-            PitchClassInternal.pitchClass letter (Offset 0)
+            PitchClass.pitchClass letter (Offset 0)
 
         SharpFlat _ letter ->
-            PitchClassInternal.pitchClass letter (Offset -1)
+            PitchClass.pitchClass letter (Offset -1)
 
 
 asNaturalOrElseSharp : PitchClass -> PitchClass
 asNaturalOrElseSharp pc =
-    case pc |> PitchClassInternal.semitones |> EnharmonicInternal.semitonesToNaturalOrAccidental of
+    case pc |> PitchClass.semitones |> Enharmonic.semitonesToNaturalOrAccidental of
         Nat letter ->
-            PitchClassInternal.pitchClass letter (Offset 0)
+            PitchClass.pitchClass letter (Offset 0)
 
         SharpFlat letter _ ->
-            PitchClassInternal.pitchClass letter (Offset 1)
+            PitchClass.pitchClass letter (Offset 1)
 
 
 equivalents : PitchClass -> List PitchClass
 equivalents pc =
-    PitchClassInternal.all |> List.filter (PitchClassInternal.semitones >> (==) (PitchClassInternal.semitones pc))
+    PitchClass.all |> List.filter (PitchClass.semitones >> (==) (PitchClass.semitones pc))
