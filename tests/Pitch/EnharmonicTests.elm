c255bde1cf215aa84850052be7e06e3da6a0519b
Leif Battermann <leif.battermann@gmail.com>
2018-10-24 23:11:48 +0200

    better test description

--- tests/Pitch/EnharmonicTests.elm
+++ tests/Pitch/EnharmonicTests.elm
@@ -17,7 +17,7 @@ import Util.PitchClassFuzzer
 all : Test
 all =
     describe "Pitch Tests"
-        [ fuzz2 Util.PitchClassFuzzer.pitchClass Util.OctaveFuzzer.octave "simple enharmonic should have same number of semitones" <|
+        [ fuzz2 Util.PitchClassFuzzer.pitchClass Util.OctaveFuzzer.octave "simple enharmonic equivalent should have same number of semitones" <|
             \pitchClass octave ->
                 let
                     pitch =
@@ -40,19 +40,19 @@ all =
                     |> PitchEnharmonic.simple
                     |> Result.map Internal.semitones
                     |> Expect.equal expectedResult
-        , test "enharmonic of Ebbb4 as natural or sharp should be C#4" <|
+        , test "enharmonic equivalent of Ebbb4 as natural or sharp should be C#4" <|
             \_ ->
                 Pitch.pitch E Pitch.tripleFlat Octave.four
                     |> Expect.equal (Pitch.pitch C Pitch.sharp Octave.four)
-        , test "enharmonic of Cbb4 as natural or sharp should be B3" <|
+        , test "enharmonic equivalent of Cbb4 as natural or sharp should be B3" <|
             \_ ->
                 Pitch.pitch C Pitch.doubleFlat Octave.four
                     |> Expect.equal (Pitch.pitch B Pitch.natural Octave.three)
-        , test "enharmonic of F###2 as natural or flat should be Ab2" <|
+        , test "enharmonic equivalent of F###2 as natural or flat should be Ab2" <|
             \_ ->
                 Pitch.pitch F Pitch.tripleSharp Octave.two
                     |> Expect.equal (Pitch.pitch A Pitch.flat Octave.two)
-        , test "enharmonic of B#7 as natural or flat should be C8" <|
+        , test "enharmonic equivalent of B#7 as natural or flat should be C8" <|
             \_ ->
                 Pitch.pitch B Pitch.sharp Octave.seven
                     |> Expect.equal (Pitch.pitch C Pitch.natural Octave.eight)
